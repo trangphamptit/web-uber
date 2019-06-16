90ada659ee6d17a009f2f99d17f7161829b0c9f1
trangphamptit <thuytrang.bcvt2015@gmail.com>
2019-06-16 23:13:45 +0700

    upload initProject

--- src/components/Section.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import "./Section.scss";
-class Section extends Component {
-  render() {
-    return <div>this is section</div>;
-  }
-}
-
-export default Section;
